97cd5bb5770eec20ebb72972bbab6237f15fd46a
Katerina <malutina.catya14@gmail.com>
2018-11-22 21:13:20 +0200

    Deserializer created

--- Lab4/src/com/company/Main.java
+++ Lab4/src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Main {
@@ -8,18 +10,37 @@ public class Main {
     public static void main(String[] args) {
 
         Weather weather = new Weather("Sunny", "clear-day", 23, 24, 32);
-        serializer(weather);
+        String fileName = serializer(weather);
+        deserializer(fileName);
     }
 
-    private static void serializer(Weather weather){
+    private static String serializer(Weather weather){
 
-        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("weather.dat")))
+        String fileName = "weather.dat";
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName)))
         {
-
             oos.writeObject(weather);
         }
         catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return fileName;
 
+    }
+
+    private static void deserializer(String fileName){
+
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName)))
+        {
+            Weather weather = (Weather) ois.readObject();
+            System.out.printf(" Sky is " + weather.getSummary() + "\n " +
+                    "Icon name: " + weather.getIcon() + " \n " +
+                    "Temperature = " + weather.getTemperature() + " degrees \n " +
+                    "Apparent as " + weather.getApparentTemperature() + " degrees \n " +
+                    "Humidity = " + weather.getHumidity() );
+
+        }
+        catch(Exception ex){
             System.out.println(ex.getMessage());
         }
 
